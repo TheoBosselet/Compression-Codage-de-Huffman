2a2a806ae1d42956972840cfb66a4b75a435e9e9
Theo Bosselet <107395881+TheoBosselet@users.noreply.github.com>
2023-06-04 17:33:44 +0200

    Update fonction_arbre.c

--- src/fonction_arbre.c
+++ src/fonction_arbre.c
@@ -18,6 +18,7 @@ void creation_arbre_huff(Liste *pile, Liste* Arbre){
         arbre *noeud_g = malloc(sizeof(arbre));
         arbre *noeud_d = malloc(sizeof(arbre));
         arbre *noeud   = malloc(sizeof(arbre));
+        arbre *noeud1   = malloc(sizeof(arbre));
         arbre *noeud2  = malloc(sizeof(arbre));
 
         if (size_liste(Arbre) ==0){
@@ -28,47 +29,55 @@ void creation_arbre_huff(Liste *pile, Liste* Arbre){
             (*noeud).gauche = noeud_g;  a = (*noeud_g).frequence;  
             (*noeud).droite = noeud_d;  b = (*noeud_d).frequence;
         }
+
+
+
         else if(size_liste(pile) > 1 && size_liste(Arbre) >0){
     printf("test 2\n");
             supprimer_pile(pile,&noeud_g); 
             supprimer_pile(pile,&noeud_d); 
-            supprimer_file(Arbre,&noeud);
+            supprimer_file(Arbre,&noeud1);
 
-            if((*noeud_d).frequence+(*noeud_g).frequence <= (*noeud).frequence){
+            if((*noeud_d).frequence+(*noeud_g).frequence <= (*noeud1).frequence){
     printf("test 3\n");
                 printf("%d\n",(*noeud_g).frequence);
-                ajouter(Arbre,noeud);
+                ajouter(Arbre,noeud1);
+
+                    afficher(pile);
+        afficher(Arbre);
                 (*noeud).gauche = noeud_g; a = (*noeud_g).frequence;   
                 (*noeud).droite = noeud_d; b = (*noeud_d).frequence;
             }
-            else if((*noeud_d).frequence+(*noeud_g).frequence > (*noeud).frequence){
+            else if((*noeud_d).frequence+(*noeud_g).frequence > (*noeud1).frequence){
     printf("test 4\n");
                 if(size_liste(Arbre)>1){
     printf("test 4.1\n");
                     supprimer_file(Arbre,&noeud2);
-                    if((*noeud2).frequence + (*noeud).frequence <= (*noeud_g).frequence + (*noeud_d).frequence){
+                    if((*noeud2).frequence + (*noeud1).frequence <= (*noeud_g).frequence + (*noeud_d).frequence){
     printf("test 4.1.1\n");
                         ajouter(pile,noeud_d); 
                         ajouter(pile,noeud_g);
-    afficher(pile);
-    afficher(Arbre);
+
     
                         
-                        (noeud_g) = (noeud);
-                        (*noeud).gauche = &noeud_g; a = (*noeud_g).frequence;
+                        (*noeud).gauche = &noeud1; a = (*noeud1).frequence;
                         (*noeud).droite = &noeud2; b = (*noeud2).frequence;
                     }
                 }
                 else{
     printf("test 4.2\n");
-                    printf("test : %d\n",(*noeud_g).frequence);
+                    
                     ajouter(pile,noeud_d);
-                    noeud_d = noeud;
+
                     (*noeud).gauche = noeud_g; a = (*noeud_g).frequence;
-                    (*noeud).droite = noeud_d; b = (*noeud_d).frequence;
+                    (*noeud).droite = noeud1; b = (*noeud1).frequence;
                 }
             }
         }
+
+
+
+
         else if(size_liste(pile) == 0){ 
     printf("test 5\n");
             if(size_liste(Arbre)>1){
@@ -79,6 +88,10 @@ void creation_arbre_huff(Liste *pile, Liste* Arbre){
                 (*noeud).droite  = noeud_d; b = (*noeud_d).frequence;
             }
         }
+
+
+
+
         else{
     printf("test 6\n");
             supprimer_pile(pile,&noeud_g);
